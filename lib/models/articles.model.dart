098e8cfe04043d2762a4b7b3c06efb9122a63da3
jaev-977 <john-977@hotmail.com>
2021-09-17 19:25:02 -0500

    agregar Models,Providers,Pages y libreria

---
+++ lib/models/articles.model.dart
@@ -0,0 +1,19 @@
+class Articulo {
+  String author;
+  String title;
+  String descriptions;
+  String url;
+  String urlToImage;
+  String publishedAt;
+  String content;
+
+  Articulo({
+    required this.author,
+    required this.title,
+    required this.descriptions,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+  });
+}
